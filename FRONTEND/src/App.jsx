48543f720a03fe6e2e41f6678c8d5aab134f12f2
Beingaditi <@Beingaditi>
2026-07-13 19:51:44 +0530

    Initial commit

---
+++ FRONTEND/src/App.jsx
@@ -0,0 +1,33 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './index.css';
+import LandingPage from "./pages/LandingPage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+import NotFoundPage from "./pages/NotFoundPage";
+
+
+function App(){
+
+  return(
+
+    <BrowserRouter>
+
+      <Routes>
+
+        <Route path="/" element={<LandingPage />} />
+
+        <Route path="/about" element={<AboutPage />} />
+
+        <Route path="/contact" element={<ContactPage />} />
+
+        <Route path="*" element={<NotFoundPage />} />
+
+      </Routes>
+
+    </BrowserRouter>
+
+  )
+
+}
+
+export default App;
---
+++ FRONTEND/src/pages/AboutPage.jsx
@@ -0,0 +1,35 @@
+import Navbar from "../components/layout/Navbar";
+import Footer from "../components/layout/Footer";
+
+
+function AboutPage(){
+
+return(
+
+<>
+
+<Navbar/>
+
+
+<h1>
+About SkillPath
+</h1>
+
+
+<p>
+SkillPath is a platform that helps students learn,
+practice and improve technical skills.
+</p>
+
+
+<Footer/>
+
+
+</>
+
+)
+
+}
+
+
+export default AboutPage;
