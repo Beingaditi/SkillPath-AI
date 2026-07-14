807b6cffcd1072fd663c48649a47cfba6857a965
Beingaditi <@Beingaditi>
2026-07-14 09:18:18 +0530

    Updated project

--- BACKEND/models/User.js
+++ BACKEND/models/User.js
@@ -1,68 +1,26 @@
-import mongoose from "mongoose";
-import bcrypt from "bcryptjs";
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema(
-    
-    {
-
-    name:{
-        type:String,
-        required: [true,'Name is required'],
-        trim:true
-    },
-
-    email:{
-
-        type:String,
-        required:[true,'email is required'],
-        trim:true,
-        unique:true,
-        lowercase:true,
-    },
-
-password:{
-
-    type:String,
-    required:[true,'password is required'],
-    minlength:[6,'Password must be atleast 6 characters'],
-
-},
-role:{
-
-    type:String,
-    enum:['user','admin'],
-    default:'user'
-},
-
-isActive:{
-
-    type:Boolean,
-    default:true,
-},
-
-    },
-
-{
-
-    timestamps:true,
-}
-
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ['user', 'admin'], default: 'user' },
+    isActive: { type: Boolean, default: true }
+  },
+  { timestamps: true }
 );
 
-userSchema.methods.matchPassword= async function( enteredPassword){
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
-    return await bcrypt.compare(enteredPassword,this.password)
-}
-
-userSchema.pre('save', async function(next){
-
-
-    if(!this.isModified(`password`)) return next();
-    const salt =await bcrypt.genSalt(10)
-    this.password = await bcrypt.hash(this.password, salt)
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
-
-const User=mongoose.model('User', userSchema);
-
+const User = mongoose.model('User', userSchema);
 export default User;
